462d3d81f8576d647a6081acfee3d0ec45e96a74
Victor Nikolaev Hristov <Victor.h7@live.com>
2020-01-26 17:01:50 +0100

    Made mainContent & sidebar look a lot nicer

---
+++ JS/app.js
@@ -0,0 +1,11 @@
+const inputLemon = document.getElementById('inputLemon');
+
+const inputUSD = document.getElementById('inputUSD');
+
+inputLemon.addEventListener('click', function() {
+	console.log('clicked');
+});
+
+inputUSD.addEventListener('click', function() {
+	console.log('clicked');
+});
